e73993ea255abc818af2a307c35897999a743393
Joe Wipperfurth <wipperfurth059@gmail.com>
2023-08-12 13:13:33 -0500

    added notes throughout that explain a bit about what is going on

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,5 @@
 console.log('***** Music Collection *****')
-
+//here we are creating adding and pushing items to the array collection
 let collection = [];
 function addToCollection ('Title', 'Artist', 'Year Published') {
     collection.push('Title', 'Artist', 'Year Published')
@@ -11,9 +11,12 @@ addToCollection ('Whats Next', 'Austin Millz, Pell', '2022');
 addToCollection ('Got Time For Me', 'Jamie Jones, Channel Tres', '2023');
 addToCollection ('Little Things', 'Jorja Smith', '2023');
 addToCollection ('Nobody But You', 'Sonder, Jorja Smith', '2021');
+//end addToCollection section
 
+//here we are just logging out the collection information
 console.log(collection);
 
+//begin showCollection section
 function showCollection (collection) {
     for (let i=0; i < collection.length; ++i)
     console.log ('number of songs listed ' + collection.length)
@@ -21,7 +24,10 @@ function showCollection (collection) {
 }
 
 showCollection();
+//end showCollection section
 
+
+//begin search by artist section
 let artistsListed = [];
 
 function findByArtist ("Artist") {
@@ -29,3 +35,4 @@ function findByArtist ("Artist") {
     artistsListed.push(collection(i(1)))
 }
 console.log(artistsListed);
+//end search by artist section
