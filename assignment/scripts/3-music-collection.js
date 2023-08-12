bad1db748bec018418174f2c703ddb44b99d0fe8
Joe Wipperfurth <wipperfurth059@gmail.com>
2023-08-12 12:49:52 -0500

    added 6 songs and logged collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,16 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-function addToCollection ('title', 'artist') {
-    collection.push('title', 'artist')
+function addToCollection ('Title', 'Artist') {
+    collection.push('Title', 'Artist')
     return true
 }
+addToCollection ('LET IT RAIN', 'Leven Kali');
+addToCollection ('Part Of Me', 'Nia Archieves');
+addToCollection ('Whats Next', 'Austin Millz, Pell');
+addToCollection ('Got Time For Me', 'Jamie Jones, Channel Tres');
+addToCollection ('Little Things', 'Jorja Smith');
+addToCollection ('Nobody But You', 'Sonder, Jorja Smith');
+
+console.log(collection);
+
