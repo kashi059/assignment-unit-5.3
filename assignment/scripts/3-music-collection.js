382d05a8de760a565e57bffc25466322f4a5fa2b
Joe Wipperfurth <wipperfurth059@gmail.com>
2023-08-12 13:00:33 -0500

    fixed added year to each collection item

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,16 +1,21 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-function addToCollection ('Title', 'Artist') {
-    collection.push('Title', 'Artist')
+function addToCollection ('Title', 'Artist', 'Year Published') {
+    collection.push('Title', 'Artist', 'Year Published')
     return true
 }
-addToCollection ('LET IT RAIN', 'Leven Kali');
-addToCollection ('Part Of Me', 'Nia Archieves');
-addToCollection ('Whats Next', 'Austin Millz, Pell');
-addToCollection ('Got Time For Me', 'Jamie Jones, Channel Tres');
-addToCollection ('Little Things', 'Jorja Smith');
-addToCollection ('Nobody But You', 'Sonder, Jorja Smith');
+addToCollection ('LET IT RAIN', 'Leven Kali', '2022');
+addToCollection ('Part Of Me', 'Nia Archieves', '2022');
+addToCollection ('Whats Next', 'Austin Millz, Pell', '2022');
+addToCollection ('Got Time For Me', 'Jamie Jones, Channel Tres', '2023');
+addToCollection ('Little Things', 'Jorja Smith', '2023');
+addToCollection ('Nobody But You', 'Sonder, Jorja Smith', '2021');
 
 console.log(collection);
 
+function showCollection (collection) {
+    for (let i=0; i < collection.length; ++i)
+    console.log ('number of songs listed ' + collection.length)
+    console.log(collection(i))
+}
